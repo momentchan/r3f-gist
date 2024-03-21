108b13f5135d439039d681017b6ad168eabdb835
momentchan <mingjyunhung@gmail.com>
2024-03-21 18:15:10 +0800

    make screen shot as a function

--- utility/ScreenCapture.js
+++
@@ -1,8 +0,0 @@
-export default class ScreenCapture {
-    Snap(name = 'Screenshot.png') {
-        const link = document.createElement('a')
-        link.setAttribute('download', name)
-        link.setAttribute('href', document.querySelector('canvas').toDataURL('image/png').replace('image/png', 'image/octet-stream'))
-        link.click()
-    }
-}
